b043886b8fc028bc30f52722ab01b7d8f15d25b3
Tri Murniati <tri.murniati2735@student.unri.ac.id>
2024-06-25 16:20:56 +0700

    Develop add tofo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,74 @@
-import React, { useState } from 'react'
-import './App.css'
-import Todos from './Todos';
+import React, { useState } from "react";
+import "./App.css";
+import Todos from "./Todos";
+import TodoForm from "./TodoForm";
 
 const todosData = [
   {
     id: 1,
-    title: 'Finish Progate React Course',
+    title: "Finish Progate React Course",
     completed: false,
   },
   {
     id: 2,
-    title: 'Have lunch with Guru Domba',
+    title: "Have lunch with Guru Domba",
     completed: false,
   },
   {
     id: 3,
-    title: 'Study React with Ninja Ken',
+    title: "Study React with Ninja Ken",
     completed: false,
   },
-]
+];
 
 function App() {
   const [todos, setTodos] = useState(todosData);
-  <link src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
 
-  console.log(todos);
+  const toggleCompleted = (todoId) => {
+    console.log(todoId);
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === todoId) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const deleteTodo = (todoId) => {
+    const updatedTodos = todos.filter((todo) => {
+      return todo.id !== todoId;
+    });
+    setTodos(updatedTodos);
+  };
+
+  const addTodo = (todoTitle) => {
+    if(todoTitle === '') {
+      return console.log("Silahkan masukkan Todo yang ingin dikerjakan!");
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos =
+    todos.concat(newTodo)
+    setTodos(updatedTodos)
+  };
 
   return (
-    <div className='container'>
+    <div className="container">
       <h1>My Todo List</h1>
-      <Todos todos={todos}/>
+      <TodoForm addTodo={addTodo} />
+      <Todos
+        todos={todos}
+        toggleCompleted={toggleCompleted}
+        deleteTodo={deleteTodo}
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/Todos.jsx
+++ src/Todos.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-export default function Todos({todos}) {
-    return(
-        <div>
-            {todos.map((todos) => {
-                return <TodoItem key={todos.id} todo={todos}/>
-            })}
-        </div>
-    );
+export default function Todos({ todos, toggleCompleted, deleteTodo }) {
+  return (
+    <div className="container">
+      {todos.map((todo) => {
+        return (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            toggleCompleted={toggleCompleted}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </div>
+  );
 }
